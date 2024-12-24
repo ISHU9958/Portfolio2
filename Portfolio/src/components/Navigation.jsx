a90222cbae320221f9efca3c21747c847b6ed87b
ISHU9958 <agishu21@gmail.com>
2024-12-24 12:08:47 +0530

    responsive

--- Portfolio/src/components/Navigation.jsx
+++ Portfolio/src/components/Navigation.jsx
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link as ScrollLink } from 'react-scroll';
 import '../styles/Navigation.css';
 
 const Navigation = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <nav className='fixed-nav'>
-         
-            <ScrollLink className='name' to="home" smooth={true} duration={1000} style={{ 'color': '#ea580c', fontSize: '35px', fontFamily: 'cursive' }}>Ishu</ScrollLink>
+            <ScrollLink className='name' to="home" smooth={true} duration={1000} style={{ color: '#ea580c', fontSize: '35px', fontFamily: 'cursive' }}>
+                Ishu
+            </ScrollLink>
 
-            <div className="navigation-link-container">
+            <button className='hamburger' onClick={toggleMenu}>
+                ☰
+            </button>
+
+            <div className={`navigation-link-container ${menuOpen ? 'open' : ''}`}>
                 <div id="about-div" className="nav-item">
-                    <ScrollLink to="about" smooth={true} duration={1000}>About</ScrollLink>
+                    <ScrollLink to="about" smooth={true} duration={1000} onClick={toggleMenu}>About</ScrollLink>
                 </div>
                 <div id="education-div" className="nav-item">
-                    <ScrollLink to="education" smooth={true} duration={1000}>Education</ScrollLink>
+                    <ScrollLink to="education" smooth={true} duration={1000} onClick={toggleMenu}>Education</ScrollLink>
                 </div>
                 <div id="skills-div" className="nav-item">
-                    <ScrollLink to="skills" smooth={true} duration={1000}>Skills</ScrollLink>
+                    <ScrollLink to="skills" smooth={true} duration={1000} onClick={toggleMenu}>Skills</ScrollLink>
                 </div>
                 <div id="projects-div" className="nav-item">
-                    <ScrollLink to="projects" smooth={true} duration={1000}>Projects</ScrollLink>
+                    <ScrollLink to="projects" smooth={true} duration={1000} onClick={toggleMenu}>Projects</ScrollLink>
                 </div>
                 <div id="contact-div" className="nav-item">
-                    <ScrollLink to="contact" smooth={true} duration={1000}>Contact</ScrollLink>
+                    <ScrollLink to="contact" smooth={true} duration={1000} onClick={toggleMenu}>Contact</ScrollLink>
                 </div>
             </div>
         </nav>
